4cb1980231b194bd8f28f50a8ce32040efc17bfc
Keaton Ufheil <keaton.ufheil@vanderbilt.edu>
2019-02-16 18:30:32 -0600

    add serialization

--- taskino.js
+++ taskino.js
@@ -1,6 +1,26 @@
 let chalk = require('chalk');
 let commander = require('commander');
 
+// setup the default task-holding object
+function initializeTaskData() {
+    let fs = require('fs');
+    let home = process.env.HOME || process.env.HOMEPATH;
+    let dataPath = `${home}/.taskino`;
+    if (!fs.existsSync(dataPath)) {
+        fs.mkdirSync(dataPath);
+        let defTaskObj= {
+            lowPriority: [],
+            medPriority: [],
+            highPriority: []
+        };
+        let serialize = require('node-serialize');
+        let objString = serialize.serialize(defTaskObj);
+        fs.writeFileSync(`${dataPath}/taskObj`, objString, err => {
+            console.log(`An error occurred: ${err}`);
+        });
+    }
+}
+
 commander
     .command('addl <task>')
     .description('Add a low priority task')
@@ -23,6 +43,6 @@ commander
     });
 
 
-commander.parse(process.argv);
+//commander.parse(process.argv);
 
-
+module.exports = { setup: initializeTaskData };
